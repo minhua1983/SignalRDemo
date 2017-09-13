f391019a6cb12f899a0c1dcfdbf6a23ace118a4a
minh <minh@vosung.cn>
2017-09-13 19:34:22 +0800

    first commit

---
+++ SignalRDemo.UI/MyHub.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web;
+using System.Data.SqlClient;
+using System.Data;
+using System.Configuration;
+using Microsoft.AspNet.SignalR;
+using Microsoft.AspNet.SignalR.Hubs;
+
+namespace SignalRDemo.UI
+{
+    /// <summary>
+    /// 自定义Hub类
+    /// </summary>
+    //[HubName("myHub")]
+    public class MyHub : Hub
+    {
+        /// <summary>
+        /// 定义一个connectionId的列表，如果MyHub是单例的话，可以不加static
+        /// </summary>
+        static List<string> connectionIdList = new List<string>();
+
+        /*
+        /// <summary>
+        /// 定义一个用户列表，实际场景时可以按Dictionary<string, UserType>类型来用string为connectionId，自定义UserType类型可以存用户类型
+        /// </summary>
+        static Dictionary<string, object> userDictionary = new Dictionary<string, object>();
+        //*/
+
+        /// <summary>
+        /// 单播发送信息
+        /// </summary>
+        /// <param name="connectionId">单播对象的connectionId</param>
+        /// <param name="message">信息的内容</param>
+        //[HubMethodName("sendOne")]
+        public void SendOne(string connectionId, string message)
+        {
+            //调用某个客户端的receiveMessage方法  
+            Clients.Client(connectionId).receiveMessage(this.Context.ConnectionId + "对" + connectionId + "说", message);
+            Clients.Client(this.Context.ConnectionId).receiveMessage(this.Context.ConnectionId + "对" + connectionId + "说", message);
+        }
+
+        /// <summary>
+        /// 广播发送信息
+        /// </summary>
+        /// <param name="message">信息的内容</param>
+        //[HubMethodName("sendAll")]
+        public void SendAll(string message)
+        {
+            //调用所有客户端的receiveMessage方法  
+            Clients.All.receiveMessage(this.Context.ConnectionId + "对大家说", message);
+        }
+
+        /// <summary>
+        /// 组播发送信息
+        /// </summary>
+        /// <param name="group">所属组名</param>
+        /// <param name="message">信息的内容</param>
+        //[HubMethodName("sendGroup")]
+        public void SendGroup(string group, string message)
+        {
+            //string group = this.Context.Headers.Get("group") ?? "";
+            //调用同组客户端的receiveMessage方法  
+            if (group != "") Clients.Group(group).receiveMessage(this.Context.ConnectionId + "对大家说", message);
+        }
+
+        /// <summary>
+        /// 当有tcp客户端第一次连接到此hub程序时
+        /// </summary>
+        /// <returns>Task对象</returns>
+        public override Task OnConnected()
+        {
+            connectionIdList.Add(this.Context.ConnectionId);
+            Clients.All.refreshConnectionIds(connectionIdList.ToArray());
+            string group = this.Context.QueryString["group"] ?? "";
+            if (group != "") this.Groups.Add(this.Context.ConnectionId, group);
+            return base.OnConnected();
+        }
+
+        /// <summary>
+        /// 当有已经连接到此hub程序的tcp客户端断开连接时
+        /// </summary>
+        /// <param name="stopCalled">是否人为断开（我用CS客户端无论人为关闭还是强制报错关闭都显示true，不太清楚如何区分何时为false）</param>
+        /// <returns>Task对象</returns>
+        public override Task OnDisconnected(bool stopCalled)
+        {
+            connectionIdList.Remove(this.Context.ConnectionId);
+            Clients.All.refreshConnectionIds(connectionIdList.ToArray());
+            string group = this.Context.QueryString["group"] ?? "";
+            if (group != "") this.Groups.Remove(this.Context.ConnectionId, group);
+            return base.OnDisconnected(stopCalled);
+        }
+
+        /// <summary>
+        /// 当服务器重新连接到客户端时（一般时IIS关闭后重启）
+        /// </summary>
+        /// <returns>Task对象</returns>
+        public override Task OnReconnected()
+        {
+            return base.OnReconnected();
+        }
+    }
+}
